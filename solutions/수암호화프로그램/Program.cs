3bcb0f4864108560f59d267cb85e23c5eea1d360
Haennim Park <66149782+mohenjo@users.noreply.github.com>
2021-08-19 13:48:45 +0900

    Add solution for 수 암호화 프로그램

---
+++ solutions/수암호화프로그램/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace 수암호화프로그램
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(GetPassword(100000));
+        }
+
+        static int GetPassword(int number) => (int)(Math.Sqrt(number) * 1_000) - number;
+    }
+}
